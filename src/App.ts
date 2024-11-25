8cbe9aa243b27bf31d72013013d84039b91f93d2
budaizalan <zalann.budai@gmail.com>
2024-11-25 10:29:07 +0100

    File read done, 2nd task done

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,27 @@
 ﻿import http from "http";
 import content from "./Content";
-
+import * as fs from 'fs';
+import * as fsPromise from 'fs/promises'; 
 class Program {
     constructor() {
         http.createServer(content).listen(process.env.PORT || 8080);
+        fileRead()
     }
 }
 
+export let trails: string[] = [];
+export let diceRolls: number[] = [];
+
+export async function fileRead() {
+    const trail = await fsPromise.open('./src/osvenyek.txt', 'r');
+    for await (const line of trail.readLines()) {
+        trails.push(line.split('\r\n').toString());
+    }
+    const diceRoll = await fsPromise.open('./src/dobasok.txt', 'r');
+    for await (const line of diceRoll.readLines()) {
+        line.replaceAll(' ', '').split('').forEach(roll => diceRolls.push(Number(roll)))
+    }
+    // console.log(trails);
+    console.log(diceRolls);
+}
 new Program();
--- src/Content.ts
+++ src/Content.ts
@@ -1,6 +1,7 @@
 ﻿import fs from "fs"; // https://nodejs.org/docs/latest-v14.x/api/fs.html
 import http from "http"; // https://nodejs.org/docs/latest-v14.x/api/http.html
 import url from "url"; // https://nodejs.org/docs/latest-v14.x/api/url.html
+import { diceRolls, fileRead, trails } from "./App";
 
 export default function content(req: http.IncomingMessage, res: http.ServerResponse): void {
     // favicon.ico kérés kiszolgálása:
@@ -24,20 +25,10 @@ export default function content(req: http.IncomingMessage, res: http.ServerRespo
 
     // Kezd a kódolást innen -->
 
-    res.write("Egyszerű Hello World! (2024/2025)\n");
-
-    // Tetszőleges html teg-ek és attribútumok beépítése:
-    res.write("<span style='color: blue;'><i>Színes és dőlt Hello World!'</i></span>\n");
-
-    // Próbáljuk számra konvertálni a "kor" paraméter (http://localhost:8080/?kor=16) értékét:
-    let korod = parseInt(params.get("kor") as string);
-    // Ha nincs "kor" paraméter megadva, vagy nem lehet számra konvertálni értékét,
-    // akkor a "korod" változóba NaN érték kerül, ilyenkor legyen 18 év az értéke:
-    if (isNaN(korod)) korod = 18;
-
-    res.write(`<label>Kérem a korod: <input type='number' name='kor' value=${korod} style='max-width:100px;' onChange='this.form.submit();'></label>\n`);
-    res.write(`Te ${korod} éves vagy!\n`);
-
+    res.write("2. feladat\n");
+    res.write(`A dobások száma: ${diceRolls.length}\n`);
+    res.write(`Az ösvények száma: ${trails.length}\n`);
+    
     // <---- Fejezd be a kódolást
 
     res.write("</pre></form></body></html>");
